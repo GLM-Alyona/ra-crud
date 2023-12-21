109a20688bbb76eaf066bb2badafb31739f0e9fb
Алёна <alyona.glm@gmail.com>
2023-12-21 13:20:12 +0300

    1 cjmmit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CRUD from './components/CRUDFunc';
+import './css/main.css';
+
+function App() {
+  return (
+    <div className="main-content">
+      <CRUD />
+    </div>
+  );
+}
+
+export default App;
